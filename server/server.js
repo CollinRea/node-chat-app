61aa6831762616b6cbb2ed5424e5c5e008664c2e
Collin Rea <collinr01@gmail.com>
2017-01-27 15:36:35 -0600

    Add message generater and test

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
@@ -16,23 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to We Chat!'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to We Chat!'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined...'
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined...'));
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
