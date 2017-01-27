0a98f5667857f81e76d5a54276edbc8f436ec7e2
Collin Rea <collinr01@gmail.com>
2017-01-27 16:43:54 -0600

    Add jquery, form for message imput and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,11 +12,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (data) {
   console.log('New message:', data);
+  var li = $('<li></li>');
+  li.text(`${data.from}: ${data.text}`)
+
+  $('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it: ', data);
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message').val()
+  }, function () {
+
+  })
 });
