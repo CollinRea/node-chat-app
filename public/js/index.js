015cc545c3562b4699bf4ed0b967163ec4de9a10
Collin Rea <collinr01@gmail.com>
2017-01-28 07:48:39 -0600

    Add generateLocationMessage and test, geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,17 @@ socket.on('newMessage', function (data) {
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (data) {
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current location</a>');
+
+  li.text(`${data.from}: `);
+  a.attr('href', data.url);
+  li.append(a);
+
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -26,5 +37,22 @@ $('#message-form').on('submit', function(e) {
     text: $('[name=message').val()
   }, function () {
 
-  })
+  });
+});
+
+var locationButton = $('#send-location')
+
+locationButton.on('click', function(e) {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, function() {
+    alert('Unable to fetch location.')
+  });
 });
